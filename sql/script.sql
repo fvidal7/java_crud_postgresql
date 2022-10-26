501ad443edcbb45f935503a50bcbd586e8a9eced
fvidal7 <felipevidalmo@gmail.com>
2022-10-26 10:05:36 -0300

    first commit

---
+++ sql/script.sql
@@ -0,0 +1,7 @@
+--criando uma tabela pra cadastro de cliente
+create table cliente(
+    idcliente       serial          primary key,
+    nome            varchar(150)    not null,
+    email           varchar(100)    not null,
+    telefone        varchar(20)     not null
+);
